bf3cac8eda6711c8009110bcae7f15156266a805
ralph rowel <ralphrowel00@gmail.com>
2026-06-11 16:58:06 +0800

    fixed bug in the mobile notice

--- js/mobile-notice.js
+++ js/mobile-notice.js
@@ -2,12 +2,3 @@ function continueToPortfolio() {
     document.getElementById('mobile-notice').style.display = 'none';
 }
 
-window.addEventListener('load', () => {
-
-    const isMobile = window.innerWidth <= 768;
-
-    if (isMobile) {
-        document.getElementById('mobile-notice').style.display = 'flex';
-    }
-
-});
